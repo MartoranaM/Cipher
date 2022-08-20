04ad1d19adf40d120e5c166d97179824e09b13d6
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-08-20 18:08:26 -0500

    Passed spaces through function

--- cipher.cpp
+++ cipher.cpp
@@ -14,22 +14,29 @@ string get_cipher_key () {
 
 string encrypt_task (string plain_text,string key, string alphabet) {
     string encrypted_text {};
-    for (char c: plain_text) 
-        encrypted_text += key[alphabet.find(c)];
+    for (char c: plain_text) {
+        if (c != ' ')
+            encrypted_text += key[alphabet.find(c)];
+        else
+            encrypted_text += c;
+    }
     return encrypted_text;
     }
 
 string decrypt_task (string encrypted_text, string key, string alphabet) {
     string decrypted_text {};
-    for (char c: encrypted_text) 
-        decrypted_text += alphabet[key.find(c)];
+    for (char c: encrypted_text) {
+        if (c != ' ')
+            decrypted_text += alphabet[key.find(c)];
+        else
+            decrypted_text += " ";
+    }
     return decrypted_text;
 }
 
 string run_encrypt () {
     string plain_text{};
     string encrypted_text{};
-
     cout << "Enter a message to be encrypted: ";
     getline(cin,plain_text);
     encrypted_text = encrypt_task(plain_text, get_cipher_key(), get_alphabet());
@@ -39,23 +46,16 @@ string run_encrypt () {
 
 string run_decrypt (string encrypted_text) {
     string decrypted_text{};
-
     cout << "Decrypting.. \n" << endl;
-
     decrypted_text = decrypt_task(encrypted_text, get_cipher_key(), get_alphabet());
     return decrypted_text;
 }
 
 int main() {
-
-
     string encrypted_text{};
     string decrypted_text{};
-
     encrypted_text = run_encrypt();
     decrypted_text = run_decrypt(encrypted_text);
-
     cout << decrypted_text;
-
     return 0;
 }
